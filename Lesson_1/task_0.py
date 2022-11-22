32533eb1af68afcfab44eb63ff93a7bc00e2559f
боков дмитрий <dbokov@rambler.ru>
2022-11-22 15:14:38 +0300

    task_1

---
+++ Lesson_1/task_0.py
@@ -0,0 +1,18 @@
+# s = 'Hello world'
+# a = 123
+# b = 456
+#
+# print(s)  # вывод строки
+# print(a, '-', b, '-', s)
+# print('{} - {} - {}'. format(a,b,s))
+# print('{0} - {1} - {2}'. format(a,b,s))
+# print(f'{a} - {b} - {s}')
+
+# Ввод и вывод данных
+# print, input
+
+print('Введите а')
+a = int(input())    # float
+print('Введите b')
+b = int(input())
+print(a, ' + ', b, ' = ', a+b)
