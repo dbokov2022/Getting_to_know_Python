f4ad82fe5fd3aa13f5cf0b3cd5a923cef29761bb
боков дмитрий <dbokov@rambler.ru>
2022-11-22 14:44:53 +0300

    hello world

---
+++ Lesson_1/task_1.py
@@ -0,0 +1,9 @@
+s = 'Hello world'
+a = 123
+b = 456
+
+print(s)  # вывод строки
+print(a, '-', b, '-', s)
+print('{} - {} - {}'. format(a,b,s))
+print('{0} - {1} - {2}'. format(a,b,s))
+print(f'{a} - {b} - {s}')
